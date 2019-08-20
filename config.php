3e4e2dff4f2b71c69d0bcc9f03974c9a5dffce0f
YouSay <yousay.sd@gmail.com>
2019-08-19 22:12:30 -0300

    configurando archivo config.php para acceder a la base de datos en Heroku

--- config.php
+++ config.php
@@ -1,27 +1,29 @@
 <?php
 // LOCALHOST
-define('RUTA', 'http://localhost/proyectos/gyarari/');
+//define('RUTA', 'http://localhost/proyectos/gyarari/');
 
-// DB
-//define('RUTA', 'https://gyarari.herokuapp.com/');
+// BASE DE DATOS SUBIDA A HEROKU
+define('RUTA', 'https://gyarari.herokuapp.com/');
 
 
-/*
+// BASE DE DATOS YA SUBIDA
 $db_config = array(
     'localhost' => 'bevx5z9fddi9vsohcjig-mysql.services.clever-cloud.com',
     'basedatos' => 'bevx5z9fddi9vsohcjig',
     'usuario' => 'uooopetimy4frvvs',
     'pass' => 't6n5mX9RMTB3QvFQ3Dta'
 );
-*/
 
+
+// LOCALHOST
+/*
 $db_config = array(
     'localhost' => 'localhost',
     'basedatos' => 'gyarari',
     'usuario' => 'root',
     'pass' => ''
 );
-
+*/
 $blog_config = array(
     'post_por_pagina' => '6',
     'carpeta_imagenes' => 'img/'
